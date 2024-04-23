0d92c4cd93d8be570ae0350034c78d130a3bbfea
BalazsManus <contact@balazsmanus.hu>
2024-04-23 17:57:12 +0200

    Add basic stuff

---
+++ extend.vb
@@ -0,0 +1,12 @@
+Namespace extends
+    Module ExMain
+        public Function InitExtensions()
+		' this initializes the extensions
+		' DO NOT REMOVE THIS OR THE APP WON'T BUILD/START
+            return true
+        End Function
+    End Module
+    module ExExtensions
+	' Recommended module for extension code
+    End module
+End Namespace
